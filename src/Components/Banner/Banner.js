c9cbc5c96e93fc38a105c662b72b19dfdabd9ea5
sahal-shalu <sahalshalu5426@gmail.com>
2023-08-23 11:06:12 +0530

    categories updated

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {API_KEY,imageUrl} from "../../constants/constants"
+import axios from '../../axios'
 import "./Banner.css"
 
+
 function Banner() {
+const [movie,setMovie]=useState()
+useEffect(()=>{
+   axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+    console.log(response.data.results[0])
+    setMovie(response.data.results[0])
+   })
+},[])
   return (
-    <div className='banner'>
+    
+    <div 
+    style={{backgroundImage:`url(${movie ? imageUrl+movie.backdrop_path:""})`}}
+    className='banner'>
         <div className='content'>
-            <h1 className='titile'>Movie Name</h1>
+            <h1 className='titile'>{movie? movie.title:" "}</h1>
             <div className='banner_buttons'>
                 <button className='button'>Play</button>
                 <button className='button'>My list</button>
             
             </div>
-            <h1 className='description'>The Lorm alphabet is a method of tactile signing named after Hieronymus Lorm, who developed it in the late 19th century.  </h1>
+            <h1 className='description'>{movie? movie.overview:""}</h1>
         </div>
         <div className="fade_bottom"></div>
     </div>
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY='cc473d4cae06e8ae9a4bb68c93a2ac7d';   
+export const imageUrl="https://image.tmdb.org/t/p/original";
